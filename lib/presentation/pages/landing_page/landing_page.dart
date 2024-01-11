307cb3cc619a8cac07aec690208b344e6e60cc82
sisosyunya <syunyasisotto@gmail.com>
2024-01-11 13:28:15 +0900

    riverpod,freezedの導入

---
+++ lib/presentation/pages/landing_page/landing_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter Demo Home Page'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
